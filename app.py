6eccbc422760e9c31ccee6e7eb4018d7b48a6f7a
kinmirai-xbarusui <xbar.usui@gmail.com>
2021-10-31 18:57:54 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -56,9 +56,12 @@ def ai_lerning():
             output_dir=st.session_state.session_dir, #The output directory
             overwrite_output_dir=True, #overwrite the content of the output directory
             num_train_epochs=epochnum, # number of training epochs
-            per_device_train_batch_size=32, # batch size for training
-            per_device_eval_batch_size=64,  # batch size for evaluation
-            per_gpu_train_batch_size=64,
+#            per_device_train_batch_size=32, # batch size for training
+#            per_device_eval_batch_size=64,  # batch size for evaluation
+#            per_gpu_train_batch_size=64,
+            per_device_train_batch_size=4, # batch size for training
+            per_device_eval_batch_size=4,  # batch size for evaluation
+            per_gpu_train_batch_size=8,
 #            eval_steps = 400, # Number of update steps between two evaluations.
             save_steps=5000, # after # steps model is saved 
 #            warmup_steps=500,# number of warmup steps for learning rate scheduler
