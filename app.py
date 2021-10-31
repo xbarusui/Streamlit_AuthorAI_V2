ba55b46efd15bfb235b7b22429a1b9537e4b5081
kinmirai-xbarusui <xbar.usui@gmail.com>
2021-10-31 19:14:58 +0900

    Add files via upload
    remove per_gpu_train_batch_size parameter will deprecated

--- app.py
+++ app.py
@@ -61,7 +61,6 @@ def ai_lerning():
 #            per_gpu_train_batch_size=64,
             per_device_train_batch_size=1, # batch size for training
             per_device_eval_batch_size=1,  # batch size for evaluation
-            per_gpu_train_batch_size=1,
 #            eval_steps = 400, # Number of update steps between two evaluations.
             save_steps=5000, # after # steps model is saved 
 #            warmup_steps=500,# number of warmup steps for learning rate scheduler
